2783491fe57a941055111d9b36148bc3701e72c4
Hector-bit <hectormartinez.2978@gmail.com>
2020-03-20 06:35:02 -0700

    algo works but is super ineffiecient

--- adv.py
+++ adv.py
@@ -13,10 +13,10 @@ world = World()
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
-map_file = "maps/test_cross.txt"
+# map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-# map_file = "maps/main_maze.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -45,22 +45,40 @@ q.enqueue(world.starting_room) #kick off the queue by adding the starting room
 while q.size() > 0:
     # dequeue a node
     something = q.dequeue()
-    print(something.id, 'SIE')
-    # Check if this node has been made if it hasn't then...
+    # Check if this node has been visited if it hasn't then...
     if something.id not in visited:
-        # Use the id to make a new node
+        # Use the id to add the node to visited
         visited[something.id] = []
-        # Check if other rooms are available
-        print(len(visited), 'asdf')
-        while len(visited) != 4:
+        # Check if all rooms have been visited
+        if len(visited) != 500:
+            # other_room is set to a list of possible exits
             other_room = player.current_room.get_exits()
-            player.travel(str(other_room[random.randrange(len(other_room))]))
+            print('OTHER ROOM - list of pos exits', other_room)
+            # random_room is set to random direction in string form
+            random_room = str(other_room[random.randrange(len(other_room))])
+            print('SHOULD BE A DIRECTION AND A STRING', type(random_room), random_room)
+            # move player to the next room based on random_room
+            player.travel(random_room)
             print(player.current_room)
+            # add the current room to the queue
             q.enqueue(player.current_room)
-            traversal_path.append(other_room)
-            print(other_room, 'OTHER ROOM  ')
+            # add the random_room direction to the traversal_path
+            traversal_path.append(random_room)
     else:
         print(f'node of {something.id} has already been visited')
+        # other_room is set to a list of possible exits
+        other_room = player.current_room.get_exits()
+        print('OTHER ROOM - list of pos exits', other_room)
+        # random_room is set to random direction in string form
+        random_room = str(other_room[random.randrange(len(other_room))])
+        print('SHOULD BE A DIRECTION AND A STRING', type(random_room), random_room)
+        # move player to the next room based on random_room
+        player.travel(random_room)
+        print(player.current_room)
+        # add the current room to the queue
+        q.enqueue(player.current_room)
+        # add the random_room direction to the traversal_path
+        traversal_path.append(random_room)
 print('visited', visited)
 
 print('YOUR TRAVERSEL PATH', traversal_path)
